60561cd7a8866ed4bcd2c48f3151044cd65b679c
parthan.v.dev <parthan.v.dev@BDC7-L-F27RD53.dir.svc.accenture.com>
2021-01-05 11:52:58 +0530

    Message added

--- Fibonaaci/src/Example/Fibonaacicode.java
+++ Fibonaaci/src/Example/Fibonaacicode.java
@@ -5,6 +5,7 @@ public class Fibonaacicode {
 	public static void main(String[] args) {
 		
 		 int n1=0,n2=1,n3,i,count=10;    
+		 System.out.println("Fibonacci series is :");
 		 System.out.print(n1+" "+n2);
 		    
 		 for(i=2;i<count;++i)    
@@ -13,6 +14,7 @@ public class Fibonaacicode {
 		  System.out.print(" "+n3);    
 		  n1=n2;    
 		  n2=n3;    
+		  
 		 }    
 		  
 
